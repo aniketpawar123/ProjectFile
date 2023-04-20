736062fae6ee0e3c6513428c13bc3807ba936b21
AniketPawar <aniketpawar979@gmail.com>
2023-04-20 22:05:07 +0530

    File is uploaded

---
+++ Angular/src/app/layouts/full/sidebar/sidebar.component.ts
@@ -0,0 +1,36 @@
+import { ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+import jwt_decode from 'jwt-decode';
+import { MenuItems } from 'src/app/shared/menu-items';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: []
+})
+export class AppSidebarComponent implements OnDestroy {
+  mobileQuery: MediaQueryList;
+  userRole:any;
+  token:any = localStorage.getItem('token');
+  tokenPayLoad:any;
+
+
+  private _mobileQueryListener: () => void;
+
+  constructor(
+    changeDetectorRef: ChangeDetectorRef,
+    media: MediaMatcher,
+    public menuItems:MenuItems
+  ) {
+    this.tokenPayLoad = jwt_decode(this.token);
+    this.userRole = this.tokenPayLoad?.role;
+
+    this.mobileQuery = media.matchMedia('(min-width: 768px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+}
